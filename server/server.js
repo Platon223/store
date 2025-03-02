a24f0d3b1d6c70d02959507edb4b9d893cd09ac8
Platon223 <platon.tikhnenko@gmail.com>
2025-03-02 10:44:40 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ app.get("/products/:name", async (req, res) => {
                     <head>
                         <meta charset="UTF-8" />
                         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <script defer src="https://store-4-rc42.onrender.com/socket.io/socket.io.js"></script>
                         <title>Document</title>
                     </head>
                     <body>
