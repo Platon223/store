b05402ef347b8b42623001d83764150cf9f324d0
Platon223 <platon.tikhnenko@gmail.com>
2025-03-12 22:27:36 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -969,23 +969,42 @@ app.get('/cart', async (req, res) => {
         cart.forEach(pr => {
             const el = document.createElement('div');
             el.className = 'cart-item';
-            el.innerHTML = \`<div class="cart-item">
+            el.innerHTML = \`
                 <img src="" alt="Product Image" />
                 <div class="cart-item-details">
                     <span class="cart-item-title">\${pr.nm}</span>
                     <span class="cart-item-price">$\${pr.price}</span>
                     <div class="cart-item-quantity">
-                    <button onclick="\${pr.q} -= 1;">-</button>
+                    <button onclick="uptadeVal(\${pr.q}, -1);">-</button>
                     <input type="text" class="quantity-input" value="\${pr.q}" />
-                    <button onclick="\${pr.q} += 1;">+</button>
+                    <button onclick="uptadeVal(\${pr.q}, 1);">+</button>
                     </div>
                 </div>
-                </div>\`;
+               \`;
 
             summary.appendChild(el);
 
-            localStorage.setItem('cart-body', JSON.stringify(cart));
+           
         });
+
+
+        function uptadeVal(name, num) {
+            const cart = JSON.parse(localStorage.getItem('cart-body'));
+        
+            const pr = cart.find(val => val.nm === name);
+        
+            pr.q += num;
+        
+            if(pr.q < 1) {
+                pr.q = 1;
+            }
+        
+            localStorage.setItem('cart-body', JSON.stringify(cart));
+        
+            location.reload();
+
+}
+
 
     
     </script>
